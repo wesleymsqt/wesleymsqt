b47a0c84a61183c8a4be16167b82691d8eaa4496
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2023-12-14 08:25:34 -0300

    DeleteALL

--- valida-acesso.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-
-    if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] == 'nao'){
-        header('Location: ../paginas/index.php?autenticaco=erro');
-    }
-?>
